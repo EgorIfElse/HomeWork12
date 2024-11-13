1e74f9ec831c2eec82b580c25dfe88b18292a18f
user <user@WIN-PRBG4ODTMQN>
2024-11-13 18:47:50 +0300

    Добавьте файлы проекта.

---
+++ HomeWork12/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork12
+{
+    static class Circle
+    {
+        public const double Pi = Math.PI;
+        static public double GetLong(double r)
+        {
+                  return 2 * Pi * r;
+        }
+        static public double GetSquare(double r)
+        {
+            return Pi * Math.Pow(r,2);
+        }
+        static public bool GetContains(double r, double x , double y)
+        {
+            return (r>x || r>y);
+        }
+        static public bool GetContainsFor(double r, double x, double y)
+        {
+            return (r == x && r == y);
+        }
+    }
+}
---
+++ HomeWork12/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.Design;
+
+namespace HomeWork12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус круга");
+            double r = Convert.ToDouble(Console.ReadLine());
+            double l = Circle.GetLong(r);
+            double s = Circle.GetSquare(r);
+            Console.WriteLine($"Длина окружности = {l},\nПлощадь круга равна {s}");
+            Console.WriteLine("Введите координаты точки Х и У для проверки ее пересечения");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            if (Circle.GetContains(r, x, y))
+            {
+                Console.WriteLine("Точка находится в круге");
+            }
+
+            else
+            {
+                if (Circle.GetContainsFor(r, x, y))
+                {
+                    Console.WriteLine("Точка находится на круге");
+                }
+                else
+                {
+                    Console.WriteLine("Точка находится за пределами круга");
+                }
+
+            }
+
+                
+            
+            
+                
+        }
+
+            
+    }
+}
